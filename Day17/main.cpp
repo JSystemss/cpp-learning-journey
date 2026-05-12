7efe920b18c575fbfd5fe8d209be0b89578243dc
JSystemss <mythicgamingacc@email.com>
2026-05-11 21:39:04 -0400

    Day17 code

--- Day17/main.cpp
+++ Day17/main.cpp
@@ -1,32 +1,21 @@
 #include <iostream>
-#include <Windows.h>
+#include <map>
+#include <string>
 
 int main()
 {
-	HWND hwnd = FindWindowA(NULL, "AssaultCube"); // This stores Assault Cube's Window Handle to my hwnd Variable
+	std::map<std::string, int> inventory;
 
-	if (!hwnd)
-	{
-		std::cout << "Window not Found!" << std::endl;
-		return 1;
-	}
+	inventory["apples"] = 50;
+	inventory["bread"] = 12;
+	inventory["milk"] = 8;
+	inventory["eggs"] = 24;
 
-	DWORD pid;
-	GetWindowThreadProcessId(hwnd, &pid); // we stored Process ID into my variable pid through my window Handle hwnd
+	std::cout << "We have " << inventory["apples"] << " Apples" << std::endl;
+	std::cout << "We have " << inventory["bread"] << " loaves of bread" << std::endl;
+	std::cout << "We have " << inventory["milk"] << " bags of milk" << std::endl;
+	std::cout << "We have " << inventory["eggs"] << " eggs" << std::endl;
 
-	if (!pid)
-	{
-		std::cout << "Could not Find Process ID" << std::endl;
-	}
-
-	HANDLE hprocess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-
-	int heath = 0;
-	ReadProcessMemory(hprocess, (LPCVOID)0x0062B7B4, &heath, sizeof(int), NULL); // I want to read my from hprocess = my opened pid with full access then i want to read from my specific heath address so i casted my hex value of my heath as a adress using windows api (LPCVOID) then i set the destination to put that in.. &heath and then we set the size of the bytes of that desitnation so it knows how much to read. and then we dont wanna know how much it read so NULL
-	std::cout << "Player heath: " << heath << std::endl;
-
-	CloseHandle(hprocess);
 	std::cin.get();
 	return 0;
-
 }
